024e027e1573dbe94827a8aabb786806f7db5de0
Gopala <guptaraja727@gmail.com>
2022-06-07 22:36:43 +0530

    commit

--- index.js
+++ index.js
@@ -228,4 +228,14 @@ var sum = 0
 for(var i=0;i<4;i++){
   sum+=arr[i]
 }
-console.log(sum);
+// console.log(sum);
+
+//////////---------------////////
+
+const ar = ['You', 'are', 'an', 'amazing', 'engineer']
+
+const sortByLength = (ar) =>{
+
+    console.log( ar.sort((a,b)=> a.length - b.length))
+}
+// sortByLength(ar)
