3ccaea596297bb26b6fd2f70af90c6b99aba365b
Gopala <guptaraja727@gmail.com>
2022-12-03 14:28:07 +0530

    start new

--- test.js
+++ test.js
@@ -17,7 +17,7 @@ for (var i = 0; i < a.length; i++) {
     obj[a[i]]++;
   }
 }
-// console.log(obj)
+console.log(obj)
 
 // useEffect(()=>{
 //   // maounting
@@ -51,6 +51,55 @@ for (var i = 0; i < a.length; i++) {
 // var d = {1:"b"}
 // console.log(s==d);
 
-let A = [1, 2, 3, 4, 5];
+// let A = [1, 2, 3, 4, 5];
 
+let Data = [
+  {
+    name: "Gopala",
+    add: "Saharsa",
+    mobile: 12457893,
+  },
+  {
+    name: "Rajesh",
+    add: "Patna",
+    mobile: 1222893,
+  },
+  {
+    name: "Rohan",
+    add: "Nauhatta",
+    mobile: 1333893,
+  },
+];
 
+// const Name = Data.map((el)=> el.name)
+
+// console.log(Name.join(" "))
+
+//  a &&  b && c {
+//     return true
+//  }
+//   A || b || c {
+//     return true
+//   }
+
+//  7 == "7"  true tyrpeof
+
+//  npx create-react-app newReactApp
+
+//  npm start
+
+let nums = [2,7,11,15], target = 22
+var twoSum = function(nums, target) {
+  //let sum = []
+  for(var i=0;i<nums.length;i++){
+    for(var j=i+1;j<nums.length;j++){
+      if(nums[i] + nums[j] === target){
+         return[i,j]
+      }
+    }
+    
+  }
+
+};
+
+// console.log(twoSum(nums, target))
