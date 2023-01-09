e30c9f6b8ec4b49cc60de8e5a6216387e6d7d13e
Gopala <guptaraja727@gmail.com>
2023-01-09 16:15:58 +0530

    commit

--- DS.js
+++ DS.js
@@ -7,12 +7,12 @@
 //3. Write a program to determine whether two matrices are equal
 //=> 4. Write a program to check if a given number is perfect cube
 
-let num = 1234
+let num = 1234;
 
 const RoundUp = (num) => {
-   return Math.round(num/50)*50
-}
-console.log(RoundUp(num))
+  return Math.round(num / 50) * 50;
+};
+// console.log(RoundUp(num));
 
 let matrix1 = [
   [1, 2, 3],
@@ -25,7 +25,32 @@ let matrix2 = [
   [8, 9, 7],
 ];
 
-const Matrix = (M1, M2) => {};
+const Matrix = (M1, M2) => {
+  const row1 = M1.length,
+    col1 = M1[0].length;
+  const row2 = M2.length,
+    col2 = M2[0].length;
+  let flag = true;
+
+  if (row1 != row2 || col1 != col2) {
+    return "Matrices are not equal";
+  } else {
+    for (let i = 0; i < row1; i++) {
+      for (let j = 0; j < col1; j++) {
+        if (M1[i][j] != M2[i][j]) {
+          flag = false;
+          break;
+        }
+      }
+    }
+  }
+  if (flag) {
+    return "Matrices are equal";
+  } else {
+    return "Matrices are not equal";
+  }
+};
+
 // console.log(Matrix(matrix1, matrix2));
 
 let inp = 27;
@@ -43,4 +68,4 @@ const CheckCube = (input) => {
   }
 };
 
-console.log(CheckCube(inp));
+// console.log(CheckCube(inp));
