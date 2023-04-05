4ba6348e9bbf9c0e828ef9934052135d03d593ae
Gopala <guptaraja727@gmail.com>
2023-04-05 19:05:55 +0530

    commit

--- Recursion.js
+++ Recursion.js
@@ -5,4 +5,20 @@ const Facto = (e) =>{
    return e * Facto(e-1)
 }
 
-console.log(Facto(5))
+// console.log(Facto(5))
+
+let money = 100
+let totalApple = 0
+const AddApple = (x) =>{
+    if(x>0){
+        buyMore(x)
+    }else{
+        console.log("money empty", totalApple);
+    }
+}
+
+const buyMore = (x) =>{
+    AddApple(x-10)
+    console.log(x);
+}
+AddApple(money)
