48e7c2e5a9068d12dfe18475e0fc2b4167fba17f
Gopala <guptaraja727@gmail.com>
2023-04-12 08:58:32 +0530

    commit

--- Recursion.js
+++ Recursion.js
@@ -40,17 +40,24 @@ for (var i=0;i<arr.length;i++){
 
 //  console.log(outnum.concat(outzro));
 
-let arr1 = [2,3,2,8,5,3]
+let arr1 = [2,4,1,1,4,7]
 // arr:[2,3,2,8,5,3] output: 2
 // 	arr: [2,4,1,1,4,7] output: 1
 // 	arr: [2,3,4] output: undefined
-let out1 = []
-for(var i=0;i<arr1.length;i++){
-    // for(var j=1;j<arr1.length;j++){
-     if(arr1[i] == arr1[i+1]){  
-        out1.push(arr1[i])
+
+  const Duplicate = (arr1) =>{
+    let out1 = []
+    for(var i=0;i<arr1.length;i++){
+        for(var j=i+1;j<arr1.length;j++){
+         if(arr1[i] == arr1[j]){  
+            out1.push(arr1[i])
+            return out1[0]
+        }else{
+            // return undefined
+        }
+        
+      } 
     }
-    
-//   } 
-}
-console.log(out1)
+  }
+
+console.log(Duplicate(arr1))
